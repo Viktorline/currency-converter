15f9ff4880333cecb80650b65e6b6adc236284a0
Victor Murzin <viktorlinework@gmail.com>
2023-05-01 13:28:54 +0700

    install dependencies, add a counter to test redux

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  parser: 'babel',
+  plugins: ['prettier-plugin-organize-imports'],
+  organizeImportsSkipDestructiveCodeActions: true,
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment } from './store/counterSlice';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hello!</p>
+    <div className="App">
+      <header className="App-header">
+        <p>Counter: {counter}</p>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
       </header>
     </div>
   );
