a47f3e2d28282ee4b2d5b4e3fbe57534dc056310
Viktorline <viktorlinework@gmail.com>
2023-05-20 03:42:36 +0700

    add black theme

--- src/application.jsx
+++ src/application.jsx
@@ -1,12 +1,19 @@
+import { ConfigProvider, theme } from 'antd';
 import { Provider } from 'react-redux';
 import AppRoutes from './AppRoutes.jsx';
 import store from './slices/store.js';
 
 const App = async () => {
   return (
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    </ConfigProvider>
   );
 };
 
